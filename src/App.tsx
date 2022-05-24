c9849279faba6bb739d40996122f49aa15e6e8a2
Tanishq Sharma <tanishqsh@gmail.com>
2022-05-24 13:58:50 -0400

    template

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="flex flex-col items-center justify-center h-screen bg-white">
+			<h1>Typescript, CRA + Tailwind out of the box</h1>
+		</div>
+	);
 }
 
 export default App;
