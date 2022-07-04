312dd8074501db749a6627bf09c1b5c0a4448c56
Tanishq Sharma <tanishqsh@gmail.com>
2022-07-04 14:42:35 -0400

    trial

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -9,7 +9,7 @@ const Home = () => {
 				<h1 className="text-5xl font-bold text-white"> Welcome to Bookworm </h1>
 				<p className="mt-4 text-white"> Review and rate books together</p>
 				<button onClick={() => navigator('/login')} className="px-4 py-3 mt-8 text-sm bg-white rounded-sm ring-2">
-					Get started with Bookworm
+					Get started with Bookworm here
 				</button>
 			</div>
 		</div>
